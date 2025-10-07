3c10b4720c59e4d67578d506a3026b3fcd0b4984
Shreyas S K <skshreyas714@gmail.com>
2025-10-07 11:57:10 +0530

    Update email address in config.js

--- src/config.js
+++ src/config.js
@@ -10,7 +10,7 @@ module.exports = {
   googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
   name: 'Shreyas S K',
   location: 'Basavanagudi, Bangalore',
-  email: 'skshreyas714@gmail.com',
+  email: 'skshreyas@zohomail.in',
   github: 'https://github.com/shreyassks',
   socialMedia: [
     {
@@ -26,7 +26,6 @@ module.exports = {
       url: 'https://www.instagram.com/ecstatic.human',
     },
   ],
-
   navLinks: [
     {
       name: 'About',
@@ -45,15 +44,12 @@ module.exports = {
       url: '/#contact',
     },
   ],
-
   navHeight: 100,
-
   colors: {
     green: '#D76E00',
     navy: '#f2ead3',
     darkNavy: '#ece1bf',
   },
-
   srConfig: (delay = 200) => ({
     origin: 'bottom',
     distance: '20px',
